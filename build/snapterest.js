86f28a6503795f5784f0990de187495d0f9ef8d0
eshandro <erik843@gmail.com>
2016-03-05 11:28:18 -0700

    Start Application module

--- build/snapterest.js
+++ build/snapterest.js
@@ -19035,93 +19035,77 @@ module.exports = require('./lib/React');
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Application = require('./components/Application.react');
 
-// Create React Elements and Render
-// var h1 = React.createElement('h1', {className: 'header', key: 'header'}, "REACT.JS");
-// var p = React.createElement('p', {className: 'content', key:'content'}, "And this is how we do it.");
-// var reactFragment = [h1, p];
-// var section = React.createElement('section', {className: 'container'}, reactFragment);
+React.render(React.createElement(Application, null), document.getElementById('react-application'));
 
-// ReactDOM.render(section, document.getElementById('react-application'));
+},{"./components/Application.react":160,"react":158,"react-dom":2}],160:[function(require,module,exports){
+"use strict";
 
-// Create React Elements using JSX
-// var headerJSX = <h2>Using JSX</h2>;
-// var listJSX =	<ul class="list-of-items">
-// <li class="list-item">Item 1</li>
-// <li class="list-item">Item 2</li>
-// <li class="list-item">Item 3</li>
-// </ul>;
-// var reactFragment = [h1, p, headerJSX, listJSX];
-// var section = React.createElement('section', {className: 'container'}, reactFragment);
+var React = require('react');
+// var Stream = require('./Stream.react');
+// var Collection = require('./Collection.react');
 
-// ReactDOM.render(section, document.getElementById('react-application'));
-
-// Create React Component
-// var ReactClass = React.createClass({
-// 	render: function() {
-// 		// console.log('this.props = ', this.props);
-// 		return React.createElement('h1', { className: 'header'}, 'ReactComponent');
-// 	}
-// });
-
-// var reactComponentElement = React.createElement(ReactClass);
-// var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
-
-// Stateful React reactComponent
-var ReactClass = React.createClass({
-	displayName: 'ReactClass',
+var Application = React.createClass({
+	displayName: "Application",
 
 	getInitialState: function getInitialState() {
 		return {
-			isHeaderHidden: false,
-			// Note: Keep the minimum items in state that you need to, e.g. the title below never changes, so no
-			// need to keep it in state. Things in State are changed by you or the user; and things in Render are
-			// updated by React
-			// title: 'Stateful React Component',
-			buttonText: 'Hide Header'
+			collectionTweets: {}
 		};
 	},
 
-	handleClick: function handleClick() {
-		console.log('handleClick fired');
-		console.log('isHeaderHidden before this.setState ', this.state.isHeaderHidden);
-		this.setState({
-			isHeaderHidden: !this.state.isHeaderHidden
-		});
-		console.log('isHeaderHidden after this.setState ', this.state.isHeaderHidden);
+	addTweetToCollection: function addTweetToCollection(tweet) {
+		var collectionTweets = this.state.collectionTweets;
 
-		console.log('this.buttonText before if ', this.state.buttonText);
-		if (!this.state.isHeaderHidden) {
-			console.log('if ', this.state.isHeaderHidden);
-			this.setState({
-				buttonText: 'Show Header'
-			});
-		} else {
-			console.log('else ', this.state.isHeaderHidden);
-			this.setState({
-				buttonText: 'Hide Header'
-			});
-		}
-		console.log('this.buttonText after if ', this.state.buttonText);
+		collectionTweets[tweet.id] = tweet;
+
+		this.setState({
+			collectionTweets: collectionTweets
+		});
+	},
+
+	removeTweetFromCollection: function removeTweetFromCollection(tweet) {
+		var collectionTweets = this.state.collectionTweets;
+
+		delete collectionTweets[tweet.id];
+
+		this.setState({
+			collectionTweets: collectionTweets
+		});
+	},
+
+	removeAllTweetsFromCollection: function removeAllTweetsFromCollection() {
+		this.setState({
+			collectionTweets: {}
+		});
 	},
 
 	render: function render() {
-		var title = 'Stateful React Component';
-		var headerElement = React.createElement('h1', { className: 'header', key: 'header' }, title);
-
-		var buttonElement = React.createElement('button', { className: 'btn btn-default', onClick: this.handleClick, key: 'button' }, this.state.buttonText);
-
-		console.log('buttonElement ', buttonElement);
-		console.log('isHeaderHidden ', this.state.isHeaderHidden);
-		if (this.state.isHeaderHidden) {
-			return React.createElement('div', null, [buttonElement]);
-		}
-
-		return React.createElement('div', null, [buttonElement, headerElement]);
+		return React.createElement(
+			"div",
+			{ className: "container-fluid" },
+			React.createElement(
+				"div",
+				{ className: "row" },
+				React.createElement(
+					"div",
+					{ className: "col-md-4 text-center" },
+					React.createElement(Stream, { onAddTweetToCollection: this.addTweetToCollection })
+				),
+				React.createElement(
+					"div",
+					{ className: "col-md-8" },
+					React.createElement(Collection, {
+						tweets: this.state.collectionTweets,
+						onRemoveTweetFromCollection: this.state.removeTweetFromCollection,
+						onRemoveAllTweetsFromCollection: this.removeAllTweetsFromCollection })
+				)
+			)
+		);
 	}
 });
 
-var reactComponentElement = React.createElement(ReactClass);
-var reactComponent = ReactDOM.render(reactComponentElement, document.getElementById('react-application'));
+module.exports = Application;
 
-},{"react":158,"react-dom":2}]},{},[159]);
+},{"react":158}]},{},[159]);
